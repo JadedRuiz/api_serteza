b13751786cbcb4435122c653248f486e62dd512c
Jaded <razonable3500@gmail.com>
2022-05-21 18:43:55 -0500

    Se añadieron nuevos datos a la boveda XML

--- app/Models/BobedaXML.php
+++ app/Models/BobedaXML.php
@@ -14,8 +14,8 @@ class BobedaXML extends Model {
     const UPDATED_AT = null;
     const CREATED_AT = null;
     protected $primaryKey = "id_bobeda";
-    protected $table = 'bobeda_xml';
+    protected $table = 'con_bovedaxml';
     protected $fillable = [
-        'id_bobeda', 'id_empresa', 'uuid', 'rfc', 'curp', 'nombre', 'num_seguro', 'tipo_combrobante', 'emitidos', 'id_estatus', 'subtotal', 'total', 'moneda', 'cambio_subtotal', 'cambio_total', 'tipo_cambio', 'descuento', 'fecha_pago', 'fecha_inicial_pago', 'fecha_final_pago', 'salario_diario', 'salario_base', 'xml', 'fecha_creacion', 'fecha_modificacion', 'usuario_creacion', 'usuario_modificacion', 'activo'
+        'id_boveda', 'id_empresa', 'uuid', 'rfc', 'curp', 'nombre', 'num_seguro', 'tipo_combrobante', 'emitidos', 'id_estatus', 'subtotal', 'total', 'moneda', 'cambio_subtotal', 'cambio_total', 'tipo_cambio', 'descuento', 'fecha_pago', 'fecha_inicial_pago', 'fecha_final_pago', 'salario_diario', 'salario_base', 'xml', 'fecha_creacion', 'fecha_modificacion', 'usuario_creacion', 'usuario_modificacion', 'activo'
     ];
 }
